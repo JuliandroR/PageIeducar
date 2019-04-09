b9a78a15ed9756828cfb5d8dbb9586aad13ed97f
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-04-08 21:34:38 -0400

    Inserção da Mensagem de serviço indisponível

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,14 @@ jQuery(function () {
       }
     });
   });
+
+
+const alertError = () => {
+  alert(
+    `Olá!
+Por Motivos de Força maior, a opção de chat do suporte encontra-se temporariamente indisponível.
+Resultando assim no oferecimento do suporte apenas por meio do preenchimento do formulário ou via Hangouts!
+\nAtenciosamente Equipe de Suporte i-Educar`)
+}
+
+alertError()
