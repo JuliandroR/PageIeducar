f29eed1b8430323ffb1fcb97193de46bc5c2ac97
JuliandroR <juliandro.ribeiro@novaandradina.org>
2019-05-09 21:31:37 -0400

    Remoção da função de contador de cliques

--- public/js/index.js
+++ public/js/index.js
@@ -1,39 +1,17 @@
-jQuery(function () {
-    jQuery(window).scroll(function () {
-      if (jQuery(this).scrollTop() > 705) {
-       $("#ieducar").css("color", "rgb(74, 22, 196)");
-      } else {
-        $("#ieducar").css("color", "aliceblue");
-      }
+jQuery(function() {
+    jQuery(window).scroll(function() {
+        if (jQuery(this).scrollTop() > 750) {
+            $("#ieducar").css("color", "rgb(74, 22, 196)");
+        } else {
+            $("#ieducar").css("color", "aliceblue");
+        }
     });
-  });
-
-  $("#doc").click(function(){
-    console.log("entro");
-    
-    jQuery.ajax({
-      url: "https://api.brunocasas.me",
-      type: "post",
-      crossDomain: false,
-      data: {},
-      success: function(response){
-        console.log("foi");
-        
-        let url = "https://docs.google.com/document/d/1WUMRnCLqHm8svP9RJep-Vaqpf-hM1lEcOVFjgiEdYWc/edit?usp=sharing"
-
-        window.location.href = url;
-        window.location.assign(url);
-      }
-      })
-  })
-
-  
-
+});
 
 
 const alertError = () => {
-  alert(
-    `Olá!
+    alert(
+        `Olá!
 Por Motivos de Força maior, a opção de chat do suporte encontra-se temporariamente indisponível.
 Resultando assim no oferecimento do suporte apenas por meio do preenchimento do formulário ou via Hangouts!
 \nAtenciosamente Equipe de Suporte i-Educar`)
